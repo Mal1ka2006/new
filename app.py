f1bc85050406846e057c0f71906114074e3154f3
Malika Axmatova <akhmatovamalika700@gmail.com>
2025-04-21 13:43:19 +0500

    app.py

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ import json, os
 from functools import wraps
 app = Flask(__name__)
 app.secret_key = 'secret_key_here'
+
+def load_json(filepath, default={}):
+    if not os.path.exists(filepath):
+        with open(filepath, 'w') as f:
+            json.dump(default, f)
+    with open(filepath, 'r') as f:
+        return json.load(f)
+def prosto():
+    q=0
+    return q
